aaf61cf65769820e6d65528f8fb1bd6d4166c5fc
emersonilan <emerson.ilan@aluno.senac.rio>
2026-04-16 20:53:53 -0300

    ajustes finais

--- cadastro/forms.py
+++ cadastro/forms.py
@@ -1,7 +1,25 @@
 from django import forms
-from .models import Pessoa
+from django.forms import inlineformset_factory
+from .models import Pessoa, Telefone
+
 
 class PessoaForm(forms.ModelForm):
     class Meta:
         model = Pessoa
         fields = ['nome', 'email', 'idade']
+
+
+class TelefoneForm(forms.ModelForm):
+    class Meta:
+        model = Telefone
+        fields = ['numero']
+
+
+# ADICIONADO (formset para múltiplos telefones)
+TelefoneFormSet = inlineformset_factory(
+    Pessoa,
+    Telefone,
+    form=TelefoneForm,
+    extra=1,
+    can_delete=True
+)
--- cadastro/views.py
+++ cadastro/views.py
@@ -1,23 +1,16 @@
 # cadastro\views.py
 
 from django.shortcuts import get_object_or_404, redirect, render
-from cadastro.forms import PessoaForm
+from cadastro.forms import PessoaForm, TelefoneFormSet
 from cadastro.models import Pessoa
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 
 def index(request):
-    # Recebe todas as pessoas do banco de dados
-    # pessoas = Pessoa.objects.all()
-
-    # Recebe todas as pessoas em ordem alfabética do nome
     pessoas = Pessoa.objects.order_by('nome')
-
-    # Total de cadastrados
     total = Pessoa.objects.count()
 
-    # Dicionário que passa dados para o template
     context = {
         'pessoas': pessoas,
         'total': total,
@@ -31,16 +24,25 @@ def contato(request):
 
 @login_required
 def adicionar(request):
-    # Só pode ser acessado por um usuátio autenticado
     if request.method == 'POST':
         form = PessoaForm(request.POST)
-        if form.is_valid():
-            form.save()
+        formset = TelefoneFormSet(request.POST)
+
+        if form.is_valid() and formset.is_valid():
+            pessoa = form.save()
+            formset.instance = pessoa
+            formset.save()
+
             messages.success(request, 'Pessoa adicionada com sucesso!')
             return redirect('index')
     else:
         form = PessoaForm()
-    return render(request, 'cadastro/adicionar.html', {'form': form})
+        formset = TelefoneFormSet()
+
+    return render(request, 'cadastro/adicionar.html', {
+        'form': form,
+        'formset': formset
+    })
 
 
 def detalhe(request, id):
@@ -50,22 +52,31 @@ def detalhe(request, id):
 
 @login_required
 def editar(request, id):
-    # Só pode ser acessado por um usuátio autenticado
     pessoa = get_object_or_404(Pessoa, id=id)
+
     if request.method == 'POST':
         form = PessoaForm(request.POST, instance=pessoa)
-        if form.is_valid():
+        formset = TelefoneFormSet(request.POST, instance=pessoa)
+
+        if form.is_valid() and formset.is_valid():
             form.save()
+            formset.save()
+
             messages.success(request, f'{pessoa.nome} atualizada com sucesso!')
             return redirect('detalhe', id=id)
     else:
         form = PessoaForm(instance=pessoa)
-    return render(request, 'cadastro/editar.html', {'form': form, 'pessoa': pessoa})
+        formset = TelefoneFormSet(instance=pessoa)
+
+    return render(request, 'cadastro/editar.html', {
+        'form': form,
+        'formset': formset,
+        'pessoa': pessoa
+    })
 
 
 @login_required
 def deletar(request, id):
-    # Só pode ser acessado por um usuátio autenticado
     pessoa = get_object_or_404(Pessoa, id=id)
     if request.method == 'POST':
         pessoa.delete()
